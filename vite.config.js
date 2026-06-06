f73dd8e69433fe9b7a8a1bd2ddedb54c71970318
tiendn1006-commits <dangtien10062003@gmail.com>
2026-06-06 15:27:05 +0700

    Configure web GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Web_GD/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Web_TTGD_LeoEducation/' : '/',
   plugins: [react()],
   build: {
     outDir: "dist",
